a9f39220cae965808fe936e6335ae1c19511acae
Telomon <thiloschenkelberg@yahoo.de>
2020-01-20 08:00:44 +0100

    Add files via upload

---
+++ graph.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class graph
+{
+    public LinkedList<node> nodes;
+
+    public void addNode(node nodeA)
+    {
+        nodes.AddLast(nodeA);
+    }
+
+}
---
+++ node.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class node
+{
+    public string name;
+    public int distance = int.MaxValue;
+
+    public LinkedList<node> shortestPath;
+
+    public Dictionary<node, int> adjacentNodes;
+
+
+    public node(string name)
+    {
+        this.name = name;
+    }
+
+
+
+    public void addDestination(node destination, int distance)
+    {
+        adjacentNodes.Add(destination, distance);
+    }
+
+    public void setDistance(int distance)
+    {
+        this.distance = distance;
+    }
+
+    public void setShortestPath(LinkedList<node> path)
+    {
+        this.shortestPath = path;
+    }  
+}
